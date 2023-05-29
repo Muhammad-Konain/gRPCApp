0b24ea157943a1273844b583777ba74674bb9bb1
Muhammad-Konain <mkonainaftab@gmail.com>
2023-05-29 21:45:46 +0500

    Getall products now returns a list of products

--- src/Server/GRPCServer/Services/ProductService.cs
+++ src/Server/GRPCServer/Services/ProductService.cs
@@ -6,11 +6,39 @@ public class ProductService : GRPCServer.ProductService.ProductServiceBase
 {
     public override Task<GetProductsResponse> GetAllProducts(GetProductRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new GetProductsResponse
+        var products = new List<ProductCatalog>
         {
-            Name = "product 1",
-            Sku = "23",
-            Price = 235
-        });
+            new ProductCatalog 
+            {
+                Name = "product 1",
+                Sku = "23",
+                Price = 235
+            },
+            new ProductCatalog 
+            {
+                Name = "product 2",
+                Sku = "223",
+                Price = 27
+            },
+            
+            new ProductCatalog 
+            {
+                Name = "product 1",
+                Sku = "23",
+                Price = 235
+            },
+            new ProductCatalog 
+            {
+                Name = "product 2",
+                Sku = "223",
+                Price = 27
+            },
+        };
+
+        var productsResponse = new GetProductsResponse();
+        productsResponse.Products.AddRange(products);
+
+        return Task.FromResult(productsResponse);
+
     }
 }
